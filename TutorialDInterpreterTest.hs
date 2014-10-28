bbf9221dc723eb57ce2f50e1449c070bc096689f
AgentM <agentm@themactionfaction.com>
2014-10-27 20:58:16 -0400

    add simple projection test in Tutorial D

--- TutorialDInterpreterTest.hs
+++ TutorialDInterpreterTest.hs
@@ -8,20 +8,22 @@ import qualified Data.Map as M
 
 main = runTestTT (TestList tests)
   where
-    tests = map (\(tutd, expected)->TestCase $ assertTutdEqual tutd expected) simpleRelTests
+    tests = map (\(tutd, expected) -> TestCase $ assertTutdEqual basicRelVarContext tutd expected) simpleRelTests
     simpleRelTests = [("true", Right relationTrue),
                       ("false", Right relationFalse),
                       ("true union false", Right relationTrue),
                       ("true; false", Right relationFalse),
                       ("a:=relation{a int}", mkRelation simpleAAttributes HS.empty),
-                      ("b:=relation{c int} rename {c as d}", mkRelation simpleBAttributes HS.empty)
+                      ("b:=relation{c int} rename {c as d}", mkRelation simpleBAttributes HS.empty),
+                      ("a:=relation{b int, c int}; a{c}", mkRelation simpleProjectionAttributes HS.empty)
                       ]
     simpleAAttributes = M.fromList [("a", Attribute "a" IntAtomType)]
     simpleBAttributes = M.fromList [("d", Attribute "d" IntAtomType)]
+    simpleProjectionAttributes = M.fromList [("c", Attribute "c" IntAtomType)]
 
 
-testSimple1 = TestCase $ assertTutdEqual "true" (Right relationTrue)
+testSimple1 = TestCase $ assertTutdEqual basicRelVarContext "true" (Right relationTrue)
 
-assertTutdEqual tutd expected = assertEqual tutd interpreted expected
+assertTutdEqual relVarContext tutd expected = assertEqual tutd interpreted expected
   where
-    interpreted = fst (interpret basicRelVarContext tutd)
+    interpreted = fst (interpret relVarContext tutd)
