6247771845c99fc6f09391506abf459d7501c8b4
AgentM <agentm@themactionfaction.com>
2015-06-30 23:15:39 -0400

    switch to advanced interface for Gnuplot graphing in anticipation of more style changes

--- ProjectM36/Relation/Show/Gnuplot.hs
+++ ProjectM36/Relation/Show/Gnuplot.hs
@@ -5,9 +5,13 @@ import ProjectM36.Relation
 import ProjectM36.Tuple
 import qualified ProjectM36.Attribute as A
 import qualified Data.Vector as V
-import qualified Graphics.Gnuplot.Simple as GP
+
 import qualified Graphics.Gnuplot.Plot.ThreeDimensional as Plot3D
 import qualified Graphics.Gnuplot.Graph.ThreeDimensional as Graph3D
+
+import qualified Graphics.Gnuplot.Plot.TwoDimensional as Plot2D
+import qualified Graphics.Gnuplot.Graph.TwoDimensional as Graph2D
+
 import qualified Graphics.Gnuplot.Advanced as GPA
 
 --this module support plotting relations containing integer attributes with arity 1,2, or 3 only
@@ -22,30 +26,25 @@ data PlotError = InvalidAttributeCountError |
 intFromAtomIndex :: Int -> RelationTuple -> Int
 intFromAtomIndex index tup = (\(IntAtom i) -> i) $ (tupleAtoms tup) V.! index
 
-pointStyle :: GP.PlotStyle
-pointStyle = GP.PlotStyle { GP.plotType = GP.Points,
-                            GP.lineSpec = GP.CustomStyle [GP.PointSize 2.0]}
-
-graph1DRelation :: Relation -> IO ()
-graph1DRelation rel = GP.plotPathStyle [] pointStyle $ map (\i -> (i, 0)) (points1DRelation rel)
+graph1DRelation :: Relation -> Plot2D.T Int Int
+graph1DRelation rel = Plot2D.list Graph2D.listPoints $ points1DRelation rel
     
 points1DRelation :: Relation -> [Int]
 points1DRelation rel = relFold folder [] rel
   where    
     folder tup acc = intFromAtomIndex 0 tup : acc
 
-graph2DRelation :: Relation -> IO ()
-graph2DRelation rel = GP.plotPathStyle [] pointStyle $ points2DRelation rel
+graph2DRelation :: Relation -> Plot2D.T Int Int
+graph2DRelation rel = Plot2D.list Graph2D.points (points2DRelation rel)
 
 points2DRelation :: Relation -> [(Int, Int)]
 points2DRelation rel = relFold folder [] rel
   where
     folder tup acc = (intFromAtomIndex 0 tup, intFromAtomIndex 1 tup) : acc
 
-graph3DRelation :: Relation -> IO ()
+graph3DRelation :: Relation -> Plot3D.T Int Int Int
 graph3DRelation rel = do
-  _ <- GPA.plotDefault $ Plot3D.cloud Graph3D.points $ points3DRelation rel
-  return ()
+  Plot3D.cloud Graph3D.points $ points3DRelation rel
                                                                          
 points3DRelation :: Relation -> [(Int, Int, Int)]                      
 points3DRelation rel = relFold folder [] rel
@@ -73,11 +72,11 @@ plotRelation :: Relation -> IO (Maybe PlotError)
 plotRelation rel = let attrTypes = V.replicate (arity rel) IntAtomType in
   if attrTypes /= A.atomTypes (attributes rel) then
     return $ Just InvalidAttributeTypeError
-  else
+  else do
     case arity rel of
-      1 -> graph1DRelation rel >> return Nothing
-      2 -> graph2DRelation rel >> return Nothing 
-      3 -> graph3DRelation rel >> return Nothing 
+      1 -> (GPA.plotDefault $ graph1DRelation rel) >> return Nothing
+      2 -> (GPA.plotDefault $ graph2DRelation rel) >> return Nothing
+      3 -> (GPA.plotDefault $ graph3DRelation rel) >> return Nothing
       _ -> return $ Just InvalidAttributeCountError
       
       
