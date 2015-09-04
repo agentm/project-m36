79224b6d08fec41b37a099cf53e7e8ce767bc42b
AgentM <agentm@themactionfaction.com>
2015-09-04 17:49:53 -0400

    WIP all fundamental PersistStore functions implemented and compiling

--- Database/Persist/ProjectM36.hs
+++ Database/Persist/ProjectM36.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE RankNTypes, TypeFamilies, GeneralizedNewtypeDeriving, OverloadedStrings, FlexibleInstances, FlexibleContexts #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Database.Persist.ProjectM36 where
-import Database.Persist
+import Database.Persist hiding (Assign, Update)
+import qualified Database.Persist as DP
 import ProjectM36.Base
 import ProjectM36.Tuple
 import qualified ProjectM36.Client as C
@@ -17,9 +18,12 @@ import qualified Data.Text as T
 import ProjectM36.Atom
 import Data.Aeson (FromJSON(..), ToJSON(..), withText)
 import qualified Data.HashSet as HS
+import qualified Data.Map as M
+import Control.Monad (when)
+import Data.Maybe (isJust)
       
 type ProjectM36Backend = C.Connection  
-
+                         
 --convert a PersistEntity to a RelationTuple
 recordAsTuple :: forall record. (PersistEntity record, PersistEntityBackend record ~ C.Connection)
             => Maybe U.UUID -> record -> Either RelationalError RelationTuple
@@ -102,7 +106,7 @@ insertNewRecords :: (Trans.MonadIO m,
                     [U.UUID] -> [record] -> ReaderT C.Connection m [Key record]
 insertNewRecords uuids records = do
   let recordsZip = zip (map Just uuids) records  
-      insertExpr = Insert (relVarName $ head records) 
+      insertExpr = Insert (relVarNameFromRecord $ head records) 
   case recordsAsRelation recordsZip of
       Left err -> throw $ PersistError (T.pack $ show err)
       Right rel -> do               
@@ -116,24 +120,18 @@ lookupByKey :: forall record m.
                 PersistEntityBackend record ~ C.Connection)
            => Key record -> ReaderT C.Connection m (Maybe (Entity record))
 lookupByKey key = do
-  case fromPersistValue $ head $ keyToValues key of
-    Left err -> Trans.liftIO $ throwIO $ PersistError "key uuid extraction failure"
-    Right matchUUID -> do
-      let matchUUIDText = T.pack $ U.toString matchUUID
-          entityInfo = entityDefFromKey key
-          keyAttributeName = unDBName (fieldDB $ entityId entityInfo)
-          relVarName = unDBName $ entityDB entityInfo
-          restrictionPredicate = AttributeEqualityPredicate keyAttributeName (NakedAtomExpr (StringAtom matchUUIDText))
-          query = Restrict restrictionPredicate (RelationVariable relVarName)
-      conn <- ask
-      resultRel <- Trans.liftIO $ C.executeRelationalExpr conn query
-      case resultRel of
-        Left err -> Trans.liftIO $ throwIO $ PersistError "executeRelationExpr error"
-        Right resultRel' -> case singletonTuple resultRel' of
-          Nothing -> return Nothing --no match on key
-          Just tuple -> do 
-            entity <- fromPersistValuesThrow entityInfo tuple --correct, one match
-            return $ Just entity
+  (relVarName, restrictionPredicate) <- commonKeyQueryProperties key
+  let query = Restrict restrictionPredicate (RelationVariable relVarName)
+  conn <- ask
+  resultRel <- Trans.liftIO $ C.executeRelationalExpr conn query
+  case resultRel of
+    Left err -> Trans.liftIO $ throwIO $ PersistError "executeRelationExpr error"
+    Right resultRel' -> case singletonTuple resultRel' of
+      Nothing -> return Nothing --no match on key
+      Just tuple -> do 
+        let entityInfo = entityDefFromKey key              
+        entity <- fromPersistValuesThrow entityInfo tuple --correct, one match
+        return $ Just entity
     
 fromPersistValuesThrow :: (Trans.MonadIO m, 
                            PersistEntity record, 
@@ -151,6 +149,63 @@ fromPersistValuesThrow entDef tuple = do
       Right key -> case body of
         Left err -> Trans.liftIO $ throwIO $ PersistError err
         Right body' -> return $ Entity key body'
+        
+commonKeyQueryProperties :: (Trans.MonadIO m, PersistEntityBackend val ~ C.Connection, PersistEntity val) => Key val -> ReaderT C.Connection m (RelVarName, RestrictionPredicateExpr)
+commonKeyQueryProperties key = do        
+  let matchUUID = keyToUUID key
+      keyAttributeName = unDBName (fieldDB $ entityId entityInfo)
+      relVarName = unDBName $ entityDB entityInfo      
+      matchUUIDText = T.pack $ U.toString matchUUID
+      entityInfo = entityDefFromKey key      
+      restrictionPredicate = AttributeEqualityPredicate keyAttributeName (NakedAtomExpr (StringAtom matchUUIDText))
+  return (relVarName, restrictionPredicate)
+        
+deleteByKey :: (Trans.MonadIO m, PersistEntityBackend val ~ C.Connection, PersistEntity val) => Key val -> ReaderT C.Connection m (Bool)
+deleteByKey key = do
+  (relVarName, restrictionPredicate) <- commonKeyQueryProperties key
+  let query = Delete relVarName restrictionPredicate
+  conn <- ask
+  maybeErr <- Trans.liftIO $ C.executeDatabaseContextExpr conn query
+  return $ isJust maybeErr
+    
+--convert persistent update list to ProjectM36 Update map    
+updatesToUpdateMap :: (Trans.MonadIO m, 
+                       PersistEntityBackend val ~ C.Connection,
+                       PersistEntity val) =>
+                      [DP.Update val] -> ReaderT C.Connection m (M.Map AttributeName Atom)
+updatesToUpdateMap updates = do 
+  let convertMap upd = case updateUpdate upd of
+        DP.Assign -> let attrName = unDBName $ fieldDB $ updateFieldDef upd 
+                         newAttrValue = persistValueAtom (updatePersistValue upd) in
+                     case newAttrValue of
+                       Nothing -> Trans.liftIO $ throwIO $ PersistError "atom conversion failed"
+                       Just newAttrValue' -> return (attrName, newAttrValue')
+        _ -> Trans.liftIO $ throwIO $ PersistError "update type not supported"
+  updateAtomList <- mapM convertMap updates
+  return $ M.fromList updateAtomList
+      
+updateByKey :: (Trans.MonadIO m, PersistEntityBackend val ~ C.Connection, PersistEntity val) => Key val -> [DP.Update val] -> ReaderT C.Connection m ()    
+updateByKey key updates = do
+  (relVarName, restrictionPredicate) <- commonKeyQueryProperties key
+  updateMap <- updatesToUpdateMap updates
+  let query = Update relVarName updateMap restrictionPredicate
+  conn <- ask
+  maybeErr <- Trans.liftIO $ C.executeDatabaseContextExpr conn query
+  case maybeErr of
+    Just err -> Trans.liftIO $ throwIO $ PersistError "executeDatabaseExpr error"
+    Nothing -> return ()
+    
+--set the truth unconditionally    
+repsertByKey :: (Trans.MonadIO m, C.Connection ~ PersistEntityBackend val, PersistEntity val) => Key val -> val -> ReaderT C.Connection m ()
+repsertByKey key val = do
+  _ <- delete key
+  insertKey key val
+    
+replaceByKey :: (Trans.MonadIO m, C.Connection ~ PersistEntityBackend val, PersistEntity val) => Key val -> val -> ReaderT C.Connection m ()     
+replaceByKey key val = do
+  deletionSuccess <- deleteByKey key
+  when (not deletionSuccess) (Trans.liftIO $ throwIO $ PersistError "entity missing during replace")
+  insertKey key val
       
 entityDefFromKey :: PersistEntity record => Key record -> EntityDef
 entityDefFromKey = entityDef . Just . recordTypeFromKey
@@ -195,10 +250,15 @@ instance PersistStore C.Connection where
       Nothing -> return Nothing
       Just (Entity _ val) -> return $ Just val
 
-  repsert = undefined
-  delete = undefined
-  replace = undefined
-  update = undefined
+  repsert = repsertByKey
+    --delete followed by insert "set the record as the truth
+    
+  delete key = deleteByKey key >> return ()
+    
+  replace = replaceByKey
+  
+  --update :: (MonadIO m, PersistEntity val, backend ~ PersistEntityBackend val) => Key val -> [Update val] -> ReaderT backend m () 
+  update = updateByKey
                   
 instance FromJSON (BackendKey C.Connection) where                  
   parseJSON = withText "ProjectM36Key" $ \t -> maybe (fail "Invalid UUID") (return . ProjectM36Key) (U.fromString (T.unpack t))
@@ -214,9 +274,9 @@ executeDatabaseContextExpr conn expr = do
     Nothing -> return ()
     Just err -> throwIO (PersistError $ T.pack (show err))
     
-relVarName :: (PersistEntity record, PersistEntityBackend record ~ C.Connection)
+relVarNameFromRecord :: (PersistEntity record, PersistEntityBackend record ~ C.Connection)
                => record -> RelVarName
-relVarName = unDBName . entityDB . entityDef . Just
+relVarNameFromRecord = unDBName . entityDB . entityDef . Just
         
 instance PersistField U.UUID where
   toPersistValue val = PersistText $ T.pack (U.toString val)
@@ -225,3 +285,11 @@ instance PersistField U.UUID where
     Just uuid -> Right uuid
   fromPersistValue _ = Left $ T.pack "expected PersistObjectId"    
     
+{- unfortunately copy-pasted from Database.Persist.Sql.Orphan.PersistStore -}
+updateFieldDef :: PersistEntity v => DP.Update v -> FieldDef
+updateFieldDef (DP.Update f _ _) = persistFieldDef f
+updateFieldDef (BackendUpdate {}) = error "updateFieldDef did not expect BackendUpdate"
+  
+updatePersistValue :: DP.Update v -> PersistValue
+updatePersistValue (DP.Update _ v _) = toPersistValue v
+updatePersistValue (BackendUpdate {}) = error "updatePersistValue did not expect BackendUpdate"
