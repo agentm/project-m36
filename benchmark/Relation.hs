b2198768f2da1777d16ab96c08bcc7e76c985080
AgentM <agentm@themactionfaction.com>
2015-10-07 21:55:19 -0400

    fix typo in benchmarking

--- benchmark/Relation.hs
+++ benchmark/Relation.hs
@@ -6,7 +6,7 @@ main = defaultMain [
   bgroup "Big Relation" [ 
      bench "100" $ whnf (matrixRelation 10) 100,
      bench "1000" $ whnf (matrixRelation 10) 1000,
-     bench "10000" $ whnf (matrixRelation 10) 10000,
+     bench "10000" $ whnf (matrixRelation 10) 10000
      ]
   ]
        
