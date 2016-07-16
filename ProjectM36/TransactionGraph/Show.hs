0748736d6a602a606ae20620b38e6f9da36d4c1a
AgentM <agentm@themactionfaction.com>
2016-07-16 14:17:37 -0400

    add basic transaction graph to graphviz digraph feature useful for debugging

--- ProjectM36/TransactionGraph/Show.hs
+++ ProjectM36/TransactionGraph/Show.hs
@@ -4,7 +4,8 @@ import ProjectM36.Base
 import ProjectM36.TransactionGraph
 import ProjectM36.Transaction
 import qualified Data.Set as S
-
+import qualified Data.Map as M
+import qualified Data.Text as T
 
 showTransactionStructure :: Transaction -> TransactionGraph -> String
 showTransactionStructure trans graph = headInfo ++ " " ++ show (transactionUUID trans) ++ " p" ++ parentTransactionsInfo ++ " c" ++ childTransactionsInfo
@@ -20,7 +21,19 @@ showGraphStructure graph@(TransactionGraph _ transSet) = S.foldr folder "" trans
   where
     folder trans acc = acc ++ showTransactionStructure trans graph ++ "\n"
     
-                              
+graphAsDot :: TransactionGraph -> String                              
+graphAsDot (TransactionGraph heads transSet) = "digraph {" ++ dot ++ headInfo ++ "}"
+  where
+    dot = S.foldr transactionAsDot "" transSet
+    transactionAsDot trans acc = acc ++ childArrows trans ++ parentArrows trans
+    arrows trans idSet = S.foldr (\c acc' -> acc' ++ oneArrow trans c) "" idSet
+    childArrows trans = arrows trans (transactionChildren trans)
+    parentArrows trans = arrows trans (transactionParentUUIDs trans)
+    transLabel t = tidLabel (transactionUUID t)
+    tidLabel l = "\"" ++ show l ++ "\""
+    oneArrow trans tid = transLabel trans ++ " -> " ++ tidLabel tid ++ ";\n"
+    headInfo = M.foldrWithKey (\headName t acc -> transLabel t ++ " [label=\"" ++ (show . transactionUUID) t ++ ":" ++ T.unpack headName ++ "\"];\n" ++ acc) "" heads
+
 
 
                              
