f155db8c77dc97fe1a911e43f8a1b21fd85948b7
AgentM <agentm@themactionfaction.com>
2016-08-21 13:05:18 -0400

    fix benchmark target

--- src/bin/benchmark/Relation.hs
+++ src/bin/benchmark/Relation.hs
@@ -1,5 +1,19 @@
 import Criterion.Main
 import ProjectM36.Relation
+import ProjectM36.Base
+import ProjectM36.Error
+import qualified ProjectM36.Attribute as A
+import qualified Data.Text as T
+import qualified Data.Vector as V
+
+-- returns a relation with tupleCount tuples with a set of integer attributes attributesCount long
+-- this is useful for performance and resource usage testing
+matrixRelation :: Int -> Int -> Either RelationalError Relation
+matrixRelation attributeCount tupleCount = do
+  let attrs = A.attributesFromList $ map (\c-> Attribute (T.pack $ "a" ++ show c) IntAtomType) [0 .. attributeCount-1]
+      tuple tupleX = RelationTuple attrs (V.generate attributeCount (\_ -> IntAtom tupleX))
+      tuples = map (\c -> tuple c) [0 .. tupleCount]
+  mkRelationDeferVerify attrs (RelationTupleSet tuples)
 
 main :: IO ()
 main = defaultMain [
