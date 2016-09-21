79f2d4270648947989f4c27b979d3e82ef1fbde4
AgentM <agentm@themactionfaction.com>
2016-09-20 22:52:38 -0400

    add fromMaybe function for Maybe types

--- src/lib/ProjectM36/DataTypes/Maybe.hs
+++ src/lib/ProjectM36/DataTypes/Maybe.hs
@@ -1,7 +1,9 @@
 module ProjectM36.DataTypes.Maybe where
 import ProjectM36.Base
+import ProjectM36.DataTypes.Primitive
 import qualified Data.HashSet as HS
 import qualified Data.Map as M
+
 
 maybeAtomType :: AtomType -> AtomType
 maybeAtomType arg = ConstructedAtomType "Maybe" (M.singleton "a" arg)
@@ -17,6 +19,11 @@ maybeAtomFunctions = HS.fromList [
   AtomFunction {
      atomFuncName ="isJust",
      atomFuncType = [maybeAtomType AnyAtomType, BoolAtomType],
-     atomFuncBody = AtomFunctionBody Nothing $ \((ConstructedAtom dConsName _ _):_) -> BoolAtom (dConsName == "Nothing")
-     }       
+     atomFuncBody = AtomFunctionBody Nothing $ \((ConstructedAtom dConsName _ _):_) -> BoolAtom (dConsName /= "Nothing")
+     },
+  AtomFunction {
+     atomFuncName = "fromMaybe",
+     atomFuncType = [AnyAtomType, maybeAtomType AnyAtomType, AnyAtomType], -- this really should be more specific, but we don't have type checker handling for this yet
+     atomFuncBody = AtomFunctionBody Nothing $ \(defaultAtom:(ConstructedAtom dConsName _ (atomVal:_)):_) -> if atomTypeForAtom defaultAtom /= atomTypeForAtom atomVal then error "poop" else if dConsName == "Nothing" then defaultAtom else atomVal
+     }
   ]
