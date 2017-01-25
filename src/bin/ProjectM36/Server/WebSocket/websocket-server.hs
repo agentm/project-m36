0bb62408d9c47c7cad7209d440358102a7dabc98
AgentM <agentm@themactionfaction.com>
2017-01-25 11:14:40 -0500

    update websocket server with MVar changes

--- src/bin/ProjectM36/Server/WebSocket/websocket-server.hs
+++ src/bin/ProjectM36/Server/WebSocket/websocket-server.hs
@@ -4,16 +4,18 @@ import ProjectM36.Server.ParseArgs
 import ProjectM36.Server
 import Control.Concurrent
 import qualified Network.WebSockets as WS
+import Network.Transport.TCP (decodeEndPointAddress)
 
 main :: IO ()
 main = do
   -- launch normal project-m36-server
-  portMVar <- newEmptyMVar
+  addressMVar <- newEmptyMVar
   serverConfig <- parseConfig
   let serverHost = bindHost serverConfig
-  _ <- forkFinally (launchServer serverConfig (Just portMVar)) failureHandler
+  _ <- forkFinally (launchServer serverConfig (Just addressMVar)) failureHandler
   --wait for server to be listening
-  serverPort <- takeMVar portMVar
+  address <- takeMVar addressMVar
+  let Just (_, serverPort, _) = decodeEndPointAddress address
   --this built-in server is apparently not meant for production use, but it's easier to test than starting up the wai or snap interfaces
-  WS.runServer "0.0.0.0" 8888 (websocketProxyServer serverPort serverHost)
+  WS.runServer "0.0.0.0" 8888 (websocketProxyServer (read serverPort) serverHost)
 
