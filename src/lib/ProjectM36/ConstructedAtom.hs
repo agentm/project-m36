4a3c45f1541991962e35bb1ef0bdc16286917048
AgentM <agentm@themactionfaction.com>
2017-03-19 22:33:02 -0400

    WIP compilation checkpoint

--- src/lib/ProjectM36/ConstructedAtom.hs
+++ src/lib/ProjectM36/ConstructedAtom.hs
@@ -6,15 +6,17 @@ import ProjectM36.Base
 import GHC.Generics
 import qualified Data.Text as T
 import qualified Data.Map as M
-import Data.Typeable
+--import Data.Typeable
+--import Debug.Trace
 
 data TestT = TestC Int
-            deriving Generic
+            deriving (Generic, Show)
                     
 data Test2T x = Test2C x
-              deriving Generic
+              deriving (Show, Generic)
                     
 instance Atomable TestT
+instance Atomable a => Atomable (Test2T a)
 
 class Atomable a where
   --type AtomT a
@@ -30,44 +32,63 @@ instance Atomable Int where
   toAtom i = IntAtom i
   toAtomType _ = IntAtomType
 
+instance Atomable Text where
+  toAtom i = TextAtom i
+  toAtomType _ = TextAtomType
+  
 -- Generics
 class AtomableG g where
   --type AtomTG g
   toAtomG :: g a -> Atom
-  toAtomTypeG :: g a -> AtomType
+  toAtomTypeG :: g a -> AtomType --overall ConstructedAtomType
+  toAtomTypesG :: g a -> [AtomType] --built from individual atom types
   
-class TypeVarMappableG g where  
-  toTypeVarMap :: g a -> TypeVarMap
-  
+{-
 instance (AtomableG a, AtomableG b) => AtomableG (a :*: b) where
   --type AtomTG (a :*: b) = (AtomTG a, AtomTG b)
   toAtomG v = undefined
   toAtomTypeG v = undefined
-  
-instance (AtomableG a, AtomableG b) => AtomableG (a :+: b) where
+-}  
+-- Right | Left  
+  {-
+instance (AtomableG (a p), AtomableG (b p)) => AtomableG ((a :+: b) p) where
   --type AtomTG (a :+: b) = (AtomTG a, AtomTG b)
   toAtomG v = undefined
   toAtomTypeG v = undefined
   
+-- Test Int Int  
+instance (Atomable (a p), Atomable (b p)) => AtomableG ((a :*: b) p) where
+  toAtomG = undefined
+  toAtomTypeG (x :*: y) = undefined
+-}
+  
 --data type metadata
-instance (Datatype c, TypeVarMappableG a) => AtomableG (M1 D c a) where  
+instance (Datatype c, AtomableG a) => AtomableG (M1 D c a) where  
   --type AtomTG (M1 D c a) = AtomTG a
   toAtomG = undefined
+  toAtomTypesG = undefined
   toAtomTypeG (M1 v) = ConstructedAtomType (T.pack typeName) tvMap
     where
+      aTypes = toAtomTypesG v
+      a2z = map T.singleton ['a'..'z']
+      tvMap = M.fromList (zip a2z aTypes)
       typeName = datatypeName (undefined :: M1 D c a x)
-      tvMap = toTypeVarMap v
-      
-instance TypeVarMappableG (M1 C c a) where
-  --M.Map TypeVarName AtomType
-  toTypeVarMap (M1 v) = M.empty
-      
-{-
-instance Atomable c => AtomableG (M1 S c (Rec0 f)) where
-  toAtomG = undefined
-  toAtomTypeG (M1 v) = toAtomType v
   
--}  
+
+instance (Constructor c, AtomableG a) => AtomableG (M1 C c a) where
+  --M.Map TypeVarName AtomType
+  --constructor name needed for Atom but not for atomType
+  toAtomG (M1 _) = ConstructedAtom (T.pack constructorName) AnyAtomType []
+    where
+      constructorName = conName (undefined :: M1 C c a x)
+  toAtomTypeG = undefined
+  toAtomTypesG (M1 v) = toAtomTypesG v
+
+instance Atomable f => AtomableG (M1 S c (Rec0 f)) where
+  toAtomG = undefined
+  toAtomTypeG = undefined
+  toAtomTypesG _ = [toAtomType (undefined :: f)]
+
 {-
 --constructor metadata
 instance AtomableG a => AtomableG (M1 C c a) where
