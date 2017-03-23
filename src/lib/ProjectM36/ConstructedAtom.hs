9519d7d7a14dd7af121a3ae7bb6e1ca5d6830fc6
AgentM <agentm@themactionfaction.com>
2017-03-23 13:29:00 -0400

    WIP compilation checkpoint database expression generation working

--- src/lib/ProjectM36/ConstructedAtom.hs
+++ src/lib/ProjectM36/ConstructedAtom.hs
@@ -3,11 +3,10 @@ module ProjectM36.ConstructedAtom where
 --http://stackoverflow.com/questions/13448361/type-families-with-ghc-generics-or-data-data
 --instances to marshal Haskell ADTs to ConstructedAtoms and back
 import ProjectM36.Base
+import ProjectM36.DataTypes.Primitive
 import GHC.Generics
 import qualified Data.Text as T
 import qualified Data.Map as M
---import Data.Typeable
---import Debug.Trace
 
 data TestT = TestC Int
             deriving (Generic, Show, Eq)
@@ -27,10 +26,9 @@ data TyTest a = TyTest a
 instance Atomable TestT
 instance Atomable a => Atomable (Test2T a)
 instance Atomable Test3T
---instance Atomable Test4T
+instance Atomable Test4T
 
 class (Eq a, Show a) => Atomable a where
-  --type AtomT a
   toAtom :: a -> Atom
   default toAtom :: (Generic a, AtomableG (Rep a)) => a -> Atom
   toAtom v = toAtomG (from v) (toAtomTypeG (from v))
@@ -39,13 +37,20 @@ class (Eq a, Show a) => Atomable a where
   default toAtomType :: (Generic a, AtomableG (Rep a)) => a -> AtomType
   toAtomType v = toAtomTypeG (from v)
   
+  --creates DatabaseContextExpr necessary to load the type constructor and data constructor into the database
+  toDatabaseContextExpr :: a -> DatabaseContextExpr
+  default toDatabaseContextExpr :: (Generic a, AtomableG (Rep a)) => a -> DatabaseContextExpr
+  toDatabaseContextExpr v = toDatabaseContextExprG (from v) (toAtomType v)
+  
 instance Atomable Int where  
   toAtom i = IntAtom i
   toAtomType _ = IntAtomType
+  toDatabaseContextExpr _ = NoOperation
 
 instance Atomable T.Text where
   toAtom i = TextAtom i
   toAtomType _ = TextAtomType
+  toDatabaseContextExpr _ = NoOperation
   
 -- Generics
 class AtomableG g where
@@ -53,49 +58,93 @@ class AtomableG g where
   toAtomG :: g a -> AtomType -> Atom
   toAtomTypeG :: g a -> AtomType --overall ConstructedAtomType
   toAtomsG :: g a -> [Atom]
-  
--- Right | Left  
-  {-
-instance (AtomableG (a p), AtomableG (b p)) => AtomableG ((a :+: b) p) where
-  --type AtomTG (a :+: b) = (AtomTG a, AtomTG b)
-  toAtomG v = undefined
-  toAtomTypeG v = undefined
-  
--- Test Int Int  
-instance (Atomable (a p), Atomable (b p)) => AtomableG ((a :*: b) p) where
-  toAtomG = undefined
-  toAtomTypeG (x :*: y) = undefined
--}
+  toDatabaseContextExprG :: g a -> AtomType -> DatabaseContextExpr
+  getConstructorsG :: g a -> [DataConstructorDef]
+  getConstructorArgsG :: g a -> [DataConstructorDefArg]
   
 --data type metadata
 instance (Datatype c, AtomableG a) => AtomableG (M1 D c a) where  
   toAtomG (M1 v) t = toAtomG v t
-  toAtomsG = undefined
+  toAtomsG = missingError
   toAtomTypeG _ = ConstructedAtomType (T.pack typeName) M.empty -- generics don't allow us to get the type constructor variables- alternatives?
     where
-      typeName = datatypeName (undefined :: M1 D c a x)
+      typeName = datatypeName (missingError :: M1 D c a x)
+  toDatabaseContextExprG (M1 v) (ConstructedAtomType tcName _) = AddTypeConstructor tcDef dataConstructors
+    where
+      tcDef = ADTypeConstructorDef tcName []
+      dataConstructors = getConstructorsG v
+  toDatabaseContextExprG _ _ = NoOperation      
+  getConstructorsG = missingError
+  getConstructorArgsG = missingError
   
-
+--constructor metadata
 instance (Constructor c, AtomableG a) => AtomableG (M1 C c a) where
   --constructor name needed for Atom but not for atomType
   toAtomG (M1 v) t = ConstructedAtom (T.pack constructorName) t atoms
     where
       atoms = toAtomsG v
-      constructorName = conName (undefined :: M1 C c a x)
-  toAtomsG = undefined
-  toAtomTypeG = undefined
+      constructorName = conName (missingError :: M1 C c a x)
+  toAtomsG = missingError
+  toAtomTypeG = missingError
+  toDatabaseContextExprG = missingError  
+  getConstructorsG (M1 v) = [DataConstructorDef (T.pack dName) dArgs]
+    where
+      dName = conName (missingError :: M1 C c a x)
+      dArgs = getConstructorArgsG v
+  getConstructorArgsG = missingError
 
+--field metadata
 instance (Selector c, AtomableG a) => AtomableG (M1 S c a) where
-  toAtomG = undefined
+  toAtomG = missingError
   toAtomsG (M1 v) = toAtomsG v
   toAtomTypeG (M1 v) = toAtomTypeG v
+  toDatabaseContextExprG _ _ = missingError  
+  getConstructorsG = error "hm"
+  getConstructorArgsG (M1 v) = getConstructorArgsG v
 
+-- field data metadata
 instance (Atomable a) => AtomableG (K1 c a) where
   toAtomG (K1 v) _ = toAtom v
   toAtomsG (K1 v) = [toAtom v]
-  toAtomTypeG (K1 v) = toAtomType (undefined :: a)
-
+  toAtomTypeG _ = toAtomType (missingError :: a)
+  toDatabaseContextExprG _ _ = missingError    
+  getConstructorsG = missingError
+  getConstructorArgsG (K1 v) = [DataConstructorDefTypeConstructorArg tCons]
+    where
+      tCons = PrimitiveTypeConstructor primitiveATypeName primitiveAType
+      primitiveAType = toAtomType v
+      primitiveATypeName = case foldr (\((PrimitiveTypeConstructorDef name typ), _) _ -> if typ == primitiveAType then Just name else Nothing) Nothing primitiveTypeConstructorMapping of
+        Just x -> x
+        Nothing -> error ("primitive type missing: " ++ show primitiveAType)
+  
+-- product types
 instance (AtomableG a, AtomableG b) => AtomableG (a :*: b) where
-  toAtomG = undefined
-  toAtomTypeG = undefined
+  toAtomG = missingError
+  toAtomTypeG = missingError
   toAtomsG (x :*: y) = toAtomsG x ++ toAtomsG y
+  toDatabaseContextExprG _ _ = missingError    
+  getConstructorsG = missingError
+  getConstructorArgsG (x :*: y) = getConstructorArgsG x ++ getConstructorArgsG y
+
+-- sum types
+instance (AtomableG a, AtomableG b) => AtomableG (a :+: b) where
+  toAtomG (L1 x) = toAtomG x
+  toAtomG (R1 x) = toAtomG x
+  toAtomTypeG = missingError
+  toAtomsG (L1 x) = toAtomsG x
+  toAtomsG (R1 x) = toAtomsG x
+  toDatabaseContextExprG _ _ = missingError  
+  getConstructorsG (L1 x) = getConstructorsG x
+  getConstructorsG (R1 x) = getConstructorsG x
+  getConstructorArgsG = missingError  
+  
+--this represents the unimplemented generics traversals which should never be called
+missingError :: a
+missingError = error "missing generics traversal"
+  
+{-
+appendExpr :: DatabaseContextExpr -> DatabaseContextExpr -> DatabaseContextExpr
+appendExpr expr (MultipleExpr l) = MultipleExpr l ++ [expr]
+appendExpr exprA exprB = MultipleExpr [exprA, exprB]
+-}
+
