89c25763ec0fb29f9809074828c6454138bc4ef4
AgentM <agentm@themactionfaction.com>
2017-08-16 23:38:05 -0400

    initial version of Simple client monad

---
+++ src/lib/ProjectM36/Client/Simple.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module ProjectM36.Client.Simple (
+  withDBConnection,
+  withTransaction,
+  executeDatabaseContextExpr,
+  executeRelationalExpr,
+  Atom(..),
+  AtomType(..),
+  DBError(..),
+  Attribute(..),
+  C.ConnectionInfo(..),
+  C.PersistenceStrategy(..),
+  C.NotificationCallback(..),
+  C.emptyNotificationCallback,
+  C.DatabaseContextExpr(..),
+  C.RelationalExprBase(..)  
+  ) where
+-- | A simplified, monad-based client for Project:M36 database access.
+import ProjectM36.Error
+import ProjectM36.Base
+import qualified ProjectM36.Client as C
+import Control.Monad.Reader
+import Control.Exception.Base
+
+-- | A union of connection and other errors that can be returned from 'withDBConnection'.
+data DBError = ConnError C.ConnectionError |
+               RelError RelationalError
+               deriving (Eq, Show)
+
+newtype DB a = DB {runDB :: ReaderT (C.SessionId, C.Connection) IO a}
+  deriving (Functor, Applicative, Monad, MonadIO)
+
+-- | Opens a connection in the 'DB' monad and closes it after running the monad.
+withDBConnection :: C.ConnectionInfo -> DB (Either DBError a) -> IO (Either DBError a)
+withDBConnection connInfo block = bracket (C.connectProjectM36 connInfo) (\eConn -> case eConn of
+                                                                             Left err -> pure (Right ())
+                                                                             Right conn -> C.close conn >> pure (Right ())) $ \eConn -> do
+  case eConn of
+    Left err -> pure (Left (ConnError err))
+    Right conn -> do
+      eSess <- C.createSessionAtHead conn "master"
+      case eSess of
+        Left err -> pure (Left (RelError err))
+        Right session ->
+           runReaderT (runDB block) (session, conn)
+           
+wrapClientIO :: (C.SessionId -> C.Connection -> IO (Either RelationalError a)) -> DB (Either DBError a)
+wrapClientIO func = DB $ do
+  (sess, conn) <- ask
+  ret <- liftIO (func sess conn)
+  case ret of
+    Left err -> pure (Left (RelError err))
+    Right val -> pure (Right val)
+           
+-- | Atomically commits the result of the database context expression to disk on the server or restores the initial state.
+withTransaction :: C.DatabaseContextExpr -> DB (Either DBError ())           
+withTransaction expr = do
+  ret <- executeDatabaseContextExpr expr
+  case ret of 
+    Left err -> pure (Left err)
+    Right _ -> do
+      eCurrentHead <- wrapClientIO C.headName
+      case eCurrentHead of
+        Left err -> pure (Left err)
+        Right currentHead ->
+          wrapClientIO (\sess conn -> C.autoMergeToHead sess conn C.UnionMergeStrategy currentHead)
+           
+-- | Execute a 'DatabaseContextExpr' in the 'DB' monad. Database context expressions manipulate the state of the database.
+executeDatabaseContextExpr :: C.DatabaseContextExpr -> DB (Either DBError ())
+executeDatabaseContextExpr expr = DB $ do
+  (sess, conn) <- ask
+  eErr <- liftIO $ C.executeDatabaseContextExpr sess conn expr
+  case eErr of
+    Left err -> pure (Left (RelError err))
+    Right () -> pure (Right ())
+    
+-- | Execute a 'RelationalExpr' in the 'DB' monad. Relational expressions perform read-only queries against the current database state.
+executeRelationalExpr :: C.RelationalExpr -> DB (Either DBError Relation)    
+executeRelationalExpr expr = wrapClientIO (\sess conn -> C.executeRelationalExpr sess conn expr)
---
+++ test/Client/Simple.hs
@@ -0,0 +1,66 @@
+import ProjectM36.Client.Simple
+import Test.HUnit
+import System.Exit
+import ProjectM36.Relation
+import qualified ProjectM36.Client as C
+import System.IO.Temp
+import System.FilePath
+import ProjectM36.TupleSet
+import ProjectM36.Attribute
+import ProjectM36.Error
+import qualified Data.Vector as V
+
+main :: IO ()           
+main = do 
+  tcounts <- runTestTT testList
+  if errors tcounts + failures tcounts > 0 then exitFailure else exitSuccess
+  
+testList :: Test
+testList = TestList [testSimpleCommitSuccess, testSimpleCommitFailure]
+
+assertEither :: (Show a) => IO (Either a b) -> IO b
+assertEither x = do
+  res <- x
+  case res of
+    Left err -> assertFailure (show err) >> undefined
+    Right val -> pure val
+
+testSimpleCommitSuccess :: Test
+testSimpleCommitSuccess = TestCase $
+  withSystemTempDirectory "m36tempdb" $ \tempdir -> do
+    let connInfo = InProcessConnectionInfo (MinimalPersistence (tempdir </> "db")) emptyNotificationCallback []
+        relExpr = Union (RelationVariable "x" ()) (RelationVariable "y" ())
+    
+    Right _ <- withDBConnection connInfo $ do
+      Right _ <- withTransaction (MultipleExpr [
+                          Assign "x" (ExistingRelation relationTrue),
+                          Assign "y" (ExistingRelation relationFalse)
+                          ])
+      executeRelationalExpr relExpr
+  
+  -- re-open with standard API and validate that the relvars are available
+    conn <- assertEither $ C.connectProjectM36 connInfo
+    
+    sess <- assertEither $ C.createSessionAtHead conn "master"
+    eRes <- C.executeRelationalExpr sess conn relExpr
+    assertEqual "x and y" (Right relationTrue) eRes
+    
+testSimpleCommitFailure :: Test
+testSimpleCommitFailure = TestCase $ do
+  let failAttrs = attributesFromList [Attribute "fail" IntAtomType]
+  err <- withSystemTempDirectory "m36tempdb" $ \tempdir -> do
+    let connInfo = InProcessConnectionInfo (MinimalPersistence (tempdir </> "db")) emptyNotificationCallback []
+    withDBConnection connInfo $ do
+      Right _ <- withTransaction (Assign "x" (ExistingRelation relationTrue))
+      --cause error
+      withTransaction (Assign "x" (MakeStaticRelation failAttrs emptyTupleSet))
+  let expectedErr = Left (RelError (RelVarAssignmentTypeMismatchError V.empty failAttrs))
+  assertEqual "dbc error" expectedErr err
+  
+
+
+
+
+    
+      
+  
