452ecb2829cd60ace1fc455e713d36921be01c4d
AgentM <agentm@themactionfaction.com>
2017-09-14 16:12:12 -0400

    make adjustments suggested by 3noch - DB -> Db - add non-cancelling version of query and execute - add withTransactionUsing to select a merge strategy

--- src/lib/ProjectM36/Client/Simple.hs
+++ src/lib/ProjectM36/Client/Simple.hs
@@ -8,7 +8,7 @@ module ProjectM36.Client.Simple (
   close,
   Atom(..),
   AtomType(..),
-  DBError(..),
+  DbError(..),
   Attribute(..),
   C.ConnectionInfo(..),
   C.PersistenceStrategy(..),
@@ -24,16 +24,16 @@ import qualified ProjectM36.Client as C
 import Control.Monad.Reader
 import Control.Exception.Base
 
-type DBConn = (C.SessionId, C.Connection)
+type DbConn = (C.SessionId, C.Connection)
 
-newtype DB a = DB {runDB :: ReaderT (C.SessionId, C.Connection) IO a}
-  deriving (Functor, Applicative, Monad)
+newtype Db a = Db {runDB :: ReaderT DbConn IO a}
+  deriving (Functor, Applicative, Monad, MonadIO)
            
-newtype TransactionCancelled = TransactionCancelled DBError deriving Show
+newtype TransactionCancelled = TransactionCancelled DbError deriving Show
 instance Exception TransactionCancelled
 
 -- | A simple alternative to 'connectProjectM36' which includes simple session management
-simpleConnectProjectM36 :: C.ConnectionInfo -> IO (Either DBError DBConn)
+simpleConnectProjectM36 :: C.ConnectionInfo -> IO (Either DbError DbConn)
 simpleConnectProjectM36 connInfo = do
   eConn <- C.connectProjectM36 connInfo
   case eConn of
@@ -46,19 +46,23 @@ simpleConnectProjectM36 connInfo = do
           pure (Left (RelError err))
         Right sess -> pure (Right (sess, conn))
         
-close :: DBConn -> IO ()        
+close :: DbConn -> IO ()        
 close (_ , conn) = C.close conn
 
--- | Runs an IO monad which may include some database updates. If an exception or error occurs, the transaction is rolled back. Otherwise, the transaction is committed.
-withTransaction :: DBConn -> DB a -> IO (Either DBError a)
-withTransaction (sess, conn) dbm = do
+-- | Runs a Db monad which may include some database updates. If an exception or error occurs, the transaction is rolled back. Otherwise, the transaction is committed to the head of the current branch.
+withTransaction :: DbConn -> Db a -> IO (Either DbError a)
+withTransaction sessconn = withTransactionUsing sessconn UnionMergeStrategy 
+
+-- | Same a 'withTransaction' except that the merge strategy can be specified.
+withTransactionUsing :: DbConn -> MergeStrategy -> Db a -> IO (Either DbError a)
+withTransactionUsing (sess, conn) strat dbm = do
   eHeadName <- C.headName sess conn
   case eHeadName of 
     Left err -> pure (Left (RelError err))
     Right headName -> do
-      let successFunc = C.autoMergeToHead sess conn UnionMergeStrategy headName
+      let successFunc = C.autoMergeToHead sess conn strat headName
           block = runReaderT (runDB dbm) (sess, conn)
-          handler :: TransactionCancelled -> IO (Either DBError a)
+          handler :: TransactionCancelled -> IO (Either DbError a)
           handler (TransactionCancelled err) = pure (Left err)
       handle handler $ do
         ret <- C.withTransaction sess conn (block >>= pure . Right) successFunc
@@ -67,32 +71,42 @@ withTransaction (sess, conn) dbm = do
           Right val -> pure (Right val)
 
 -- | A union of connection and other errors that can be returned from 'withDBConnection'.
-data DBError = ConnError C.ConnectionError |
+data DbError = ConnError C.ConnectionError |
                RelError RelationalError |
                TransactionRolledBack
                deriving (Eq, Show)
 
 -- | Execute a 'DatabaseContextExpr' in the 'DB' monad. Database context expressions manipulate the state of the database. In case of an error, the transaction is terminated and the connection's session is rolled back.
-execute :: C.DatabaseContextExpr -> DB ()
-execute expr = DB $ do
-  (sess, conn) <- ask
-  ret <- liftIO $ C.executeDatabaseContextExpr sess conn expr
+execute :: C.DatabaseContextExpr -> Db ()
+execute expr = do
+  ret <- executeOrErr expr
   case ret of
     Left err -> liftIO $ cancelTransaction (RelError err)
     Right _ -> pure ()
     
 -- | Run a 'RelationalExpr' query in the 'DB' monad. Relational expressions perform read-only queries against the current database state.
-query :: C.RelationalExpr -> DB Relation
-query expr = DB $ do
-  (sess, conn) <- ask  
-  ret <- lift $ C.executeRelationalExpr sess conn expr
+query :: C.RelationalExpr -> Db Relation
+query expr = do
+  ret <- queryOrErr expr
   case ret of
-    Left err -> liftIO $ cancelTransaction (RelError err) >> error "impossible"
+    Left err -> liftIO $ cancelTransaction (RelError err)
     Right rel -> pure rel
+    
+-- | Run a 'DatabaseContextExpr' update expression. If there is an error, just return it without cancelling the current transaction.
+executeOrErr :: C.DatabaseContextExpr -> Db (Either RelationalError ())
+executeOrErr expr = Db $ do
+  (sess, conn) <- ask  
+  lift $ C.executeDatabaseContextExpr sess conn expr
+  
+-- | Run a 'RelationalExpr' query expression. If there is an error, just return it without cancelling the transaction.
+queryOrErr :: C.RelationalExpr -> Db (Either RelationalError Relation)
+queryOrErr expr = Db $ do
+  (sess, conn) <- ask
+  lift $ C.executeRelationalExpr sess conn expr
 
--- | Unconditionally roll back the current transaction and throw an exception to terminate the execution of the DB monad.
-rollback :: DB ()
-rollback = DB $ lift $ cancelTransaction TransactionRolledBack
+-- | Unconditionally roll back the current transaction and throw an exception to terminate the execution of the Db monad.
+rollback :: Db ()
+rollback = Db $ lift $ cancelTransaction TransactionRolledBack
 
-cancelTransaction :: DBError -> IO ()
+cancelTransaction :: DbError -> IO a
 cancelTransaction err = throwIO (TransactionCancelled err)
