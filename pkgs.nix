70a47372b57115a76f99558fc38be061226d4ccd
iko <ilyakooo0@gmail.com>
2021-05-30 14:24:23 +0300

    Improved build

--- pkgs.nix
+++
@@ -1,7 +0,0 @@
-let
-nixpkgs = builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/20.09.tar.gz";
-  sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
-};
-in
-import nixpkgs { config.allowBroken = true; }
