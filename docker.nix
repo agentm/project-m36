a659f31fc09b244c640002808969d6982135a732
AgentM <agentm@themactionfaction.com>
2023-12-30 10:14:31 -0500

    fix docker warning

--- docker.nix
+++ docker.nix
@@ -9,7 +9,7 @@ in
 pkgs.dockerTools.buildImage {
   name = "project-m36";
   tag = "latest";
-  contents = [ static-project-m36 ];
+  copyToRoot = [ static-project-m36 ];
   # expose default project-m36 and websocket server ports
   config = {
     Env = [ "LC_ALL=en_US.UTF-8" ];
